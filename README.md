a349ae0e634b67f3d68b64046a0f7aa3afb15fe8
Nayanp35 <prajapatinayan35@gmail.com>
2023-09-27 19:41:46 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# To Do App with Drag and Drop
+# Created by Nayan Prajapati
+
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div className="text-green-500">App</div>
+  )
+}
+
+export default App
