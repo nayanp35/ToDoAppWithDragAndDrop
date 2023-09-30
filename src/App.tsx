9266d941e56b25c3314df7089f029bdbe8d8c522
Nayanp35 <prajapatinayan35@gmail.com>
2023-09-30 18:06:04 -0400

    Render components in one page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import Done from './components/Done';
+import InProgress from './components/InProgress';
+import ToDo from './components/ToDo';
+import ToDoForm from './components/ToDoForm';
+
 const App = () => {
   return (
-    <div className="text-green-500">App</div>
-  )
-}
+    <div className=''>
+      <ToDoForm />
+      <div className="grid grid-cols-3 gap-5 p-5">
+        <ToDo />
+        <InProgress />
+        <Done />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
